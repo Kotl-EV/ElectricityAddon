f7a7a2e92c99a89964416c9ec0082841bfe8529b
tehtelev <tehtelev@gmail.com>
2025-03-29 20:31:06 +0300

    Обновлена модель выключателя. Вычищен старый мусор

--- ElectricityAddon/ElectricityAddon.cs
+++ ElectricityAddon/ElectricityAddon.cs
@@ -21,7 +21,7 @@ using Vintagestory.API.Common;
     "electricityaddon",
     Website = "https://github.com/Kotl-EV/ElectricityAddon",
     Description = "Brings electricity into the game!",
-    Version = "0.0.18",
+    Version = "0.0.20",
     Authors = new[] {
         "Kotl"
     }
